7ebedda0517c79ee1e33eed5dd374ca20332309e
Leo Fahrbach <leo.fahrbach@stusta.de>
2022-12-04 16:00:03 +0100

    2022 Day 2

--- 2022/solution.py
+++ 2022/solution.py
@@ -6,5 +6,15 @@ def day1():
     print(sum(sorted(elves)[-3:]))
 
 
+def day2():
+    input = [[x.split(" ")[0], x.split(" ")[1]] for x in open("02.txt").read().strip().split("\n")]
+    score_selected = sum(ord(x) - 87 for _, x in input)
+    score_won = 3 * sum((ord(y) - ord(x) + 2) % 3 for x, y in input)
+    print(score_won + score_selected)
+    score_won_2 = 3 * sum(ord(x) - 88 for _, x in input)
+    score_selected_2 = sum((ord(y) + ord(x) - 1) % 3 + 1 for x, y in input)
+    print(score_won_2 + score_selected_2)
+
+
 if __name__ == "__main__":
-    day1()
+    day2()
