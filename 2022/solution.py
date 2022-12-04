8a175b03faa09ecfc34cd657368e45ac6d14a393
Leo Fahrbach <leo.fahrbach@stusta.de>
2022-12-04 16:52:48 +0100

    2022 Day 3

--- 2022/solution.py
+++ 2022/solution.py
@@ -16,5 +16,17 @@ def day2():
     print(score_won_2 + score_selected_2)
 
 
+def day3():
+    from itertools import islice
+    def prio(x): return ord(x) - (38 if ord(x) < ord('a') else 96)
+    rucksacks = [[set(line[:(len(line)//2)]), set(line[len(line)//2:])] for line in open("03.txt").read().strip().split("\n")]
+    both = [list(x.intersection(y))[0] for x, y in rucksacks]
+    print(sum(prio(x) for x in both ))
+    input = iter(line for line in open("03.txt").read().strip().split("\n"))
+    groups = [[set(x) for x  in islice(input, 3)] for i in range(len(rucksacks)//3)]
+    keys = [list(x.intersection(y).intersection(z))[0] for x, y, z in groups]
+    print(sum(prio(x) for x in keys ))
+
+
 if __name__ == "__main__":
-    day2()
+    day3()
